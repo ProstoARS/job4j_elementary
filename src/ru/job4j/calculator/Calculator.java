785023536d202da437b7839e91956c1ef632cfe8
ProstoARS <arsudakov@gmail.com>
2021-02-03 17:52:41 +0300

    9.1. String [#459662]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
         int onePlusTwo = one + two;
         System.out.println(onePlusTwo);
 
-        int sixDivTwo = six/two;
+        int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
 
