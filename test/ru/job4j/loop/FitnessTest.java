f93748ad0a9219ace5bcae7cd913dc1346e90f7b
ProstoARS <arsudakov@gmail.com>
2021-02-08 23:11:38 +0300

    5.4. Протеиновая диета [#459709]

--- test/ru/job4j/loop/FitnessTest.java
+++ test/ru/job4j/loop/FitnessTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.loop;
 
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -22,5 +23,11 @@ public class FitnessTest {
     public void whenIvanLessByFewNik() {
         int month = Fitness.calc(50, 90);
         assertThat(month, is(2));
+    }
+
+    @Test
+    public void whenIvanEqualByFewNik() {
+        int month = Fitness.calc(90, 90);
+        assertThat(month, is(1));
     }
 }
