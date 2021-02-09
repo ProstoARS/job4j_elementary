e3dbab27e79a0fc10a9c76c5009facf3546d4e6f
ProstoARS <arsudakov@gmail.com>
2021-02-09 19:54:53 +0300

    6.0.2. Заполнение массива. [#459633]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,18 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива sumames равен: " + sumames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        System.out.println();
+
+        String[] names = new String[4];
+        names[0] = "Arseniy Sudakov";
+        names[1] = "Vladimir Budaev";
+        names[2] = "Pablo Escobar";
+        names[3] = "Staruha Shipoklyak";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
