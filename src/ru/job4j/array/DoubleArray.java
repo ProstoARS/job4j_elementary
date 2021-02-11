f22a8fe611a10cea3a4ccca9a79da990968bd95e
ProstoARS <arsudakov@gmail.com>
2021-02-11 11:54:10 +0300

    6.6.4. Размер элемента двухмерного массива. [#459645]

---
+++ src/ru/job4j/array/DoubleArray.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class DoubleArray {
+    public static void main(String[] args) {
+        int[][] array = {{5}, {3, 4}, {8, 2, 4}, {6, 3, 5, 7}};
+        System.out.println(array[0].length);
+        System.out.println(array[1].length);
+        System.out.println(array[2].length);
+        System.out.println(array[3].length);
+    }
+}
