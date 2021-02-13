f566bad174a553e849db53de3efb87adcfa1bcf0
ProstoARS <arsudakov@gmail.com>
2021-02-13 14:02:22 +0300

    Дополнительная задача к экзамену на базовый синтаксис.

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -5,8 +5,8 @@ public class Merge {
         int[] rsl = new int[left.length + right.length];
         int j = 0, k = 0;
         for (int i = 0; i < rsl.length; i++) {
-//            for (int j = 0; j < rsl.length; j++) {
-                if (j < left.length  && k < right.length && left[j] < right[k]) {
+
+                if (j < left.length  && left[j] <= right[k]) {
                     rsl[i] = left[j];
                     j++;
                 } else {
